9318738ec1a378c83bb078beca7ce034f6626db2
Jesse Hummel <jhummel@tudelft.nl>
2024-09-05 18:58:36 +0200

    A little tweak to the examples.

--- src/examples.m
+++ src/examples.m
@@ -1,5 +1,6 @@
-%% Add path to src
+%% Setup
 % This assumes that the working directory is preplot-postplot.
+close all; clearvars; clc;
 addpath('src')
 
 %% Simple example.
@@ -11,19 +12,34 @@ postplot()
 %% With some customization.
 colors = [0.15, 0.34, 0.43;
           0.42, 0.76, 0.61];
-[f, axs] = preplot(2, 1, 'paperFormat', 'WES', 'column', 1, ...
-    'lineFrac', 0.9, 'aspectRatio', 1.2, 'colororder', colors);
+[f, axs] = preplot(2, 1, 'paperFormat', 'WES', 'column', 2, ...
+    'lineFrac', 0.9, 'aspectRatio', 1.5, 'colororder', colors, ...
+    'interpreter', 'latex');
 
-t = linspace(0, 5, 1e5);
-plot(axs(1), t, sin(t), 'LineWidth', 2)
-plot(axs(1), t, cos(t), 'LineWidth', 2)
-plot(axs(2), t, log10(t), 'LineWidth', 2)
-plot(axs(2), t, log(t), 'LineWidth', 2)
-
+x = linspace(0, 5, 1e5);
+plot(axs(1), x, [sin(x); cos(x)])
+plot(axs(2), x, [log10(x); log(x)])
+ylabel(axs(1), '$$\alpha$$')
+ylabel(axs(2), '$$\beta$$')
 xlabel(axs(2), 'Time (s)')
 
-postplot(f, 'fontname', 'Monospaced', 'fontSize', 10, ...
-    'legendFontSize', 8, 'sharex', true);
+postplot(f, 'Images/test.pdf', 'fontSize', 16, 'legendFontSize', 14, ...
+    'sharex', true, 'lineWidth', 3);
+
+
+%% Control plots.
+% Plots from the control toolbox don't work together with TiledLayout and
+% are difficult to customize, so this toolbox also provides custom control
+% plotting commands.
+[f, axs] = preplot(2, 2);
+axBig = nexttile(2, [2,1]);
+
+sys = tf(4, [1, 0.7, 4]);
+mbode(sys, axs(1,1), axs(2,1))
+mnyquist(sys, axBig, 'w', logspace(-1, 2, 1e3))
+xlim(axBig, [-2, 2])
+
+postplot(f, 'sharex', true, 'linewidth', 1, 'fontname', 'NimbusRomNo9L');
 
 
 %% Logarithmic plot
@@ -37,20 +53,6 @@ plot(1:10)
 % Or:
 preplot('hold', 'off');
 loglog(1:10)
-
-
-%% Control plots.
-% Plots from the control toolbox don't work together with TiledLayout and
-% are difficult to customize, so this toolbox also provides custom control
-% plotting commands.
-[f, axs] = preplot(2, 2, 'interpreter', 'Latex');
-axBig = nexttile(2, [2,1]);
-
-sys = tf(4, [1, 0.7, 4]);
-mbode(sys, axs(1,1), axs(2,1))
-mnyquist(sys, axBig, 'w', logspace(-1, 2, 1e3))
-
-postplot('sharex', true);
 
 
 %% Example plot for README.
