9c19437b6e1e88ca04922fb0b1ba599d0f478654
Jesse Hummel <jhummel@tudelft.nl>
2024-11-14 17:17:13 +0100

    Update examples.

--- src/examples.m
+++ src/examples.m
@@ -13,9 +13,9 @@ postplot();
 
 %% With some customization.
 colors = [0.15, 0.34, 0.43;
-          0.42, 0.76, 0.61];
-[f, axs] = preplot(2, 1, 'colororder', colors, 'sharex', 'all', ...
-    'interpreter', 'latex');
+    0.42, 0.76, 0.61];
+[f, axs] = preplot(2, 1, 'sharex', 'all', 'interpreter', 'latex', ...
+    'colororder', colors);
 
 x = linspace(0, 5, 1e5);
 plot(axs(1), x, [sin(x); cos(x)])
@@ -27,6 +27,11 @@ xlabel(axs(2), 'Time (s)')
 postplot(f, 'Images/test.pdf', 'width', 5, 'figSizeUnits', 'inches', ...
     'aspectRatio', 1.5, 'fontSize', 16, 'legendFontSize', 14, ...
     'lineWidth', 3);
+% We can also save the same plot twice in different sizes, for example when
+% you want to use it in a presentation and a paper.
+postplot(f, 'Images/test2.pdf', 'width', 10, 'figSizeUnits', 'inches', ...
+    'aspectRatio', 1.5, 'fontSize', 20, 'legendFontSize', 16, ...
+    'lineWidth', 5);
 
 
 %% Control plots
@@ -61,8 +66,9 @@ loglog(ax, 1:10)
 
 %% Example plot for README.
 colors = [0.15, 0.34, 0.43;
-          0.42, 0.76, 0.61];
-[f, axs] = preplot(2, 1, 'colororder', colors, 'interpreter', 'latex', 'sharex', 'all');
+    0.42, 0.76, 0.61];
+[f, axs] = preplot(2, 1, 'colororder', colors, 'interpreter', 'latex', ...
+    'sharex', 'all');
 
 sys1 = tf(4, [1, 0.7, 4]);
 sys2 = tf([1, 0.1, 4], [1, 1, 4]);
